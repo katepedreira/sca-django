bf7714261c48e9c9cb966c9f22bde02b038d4d27
Katherine.Pedreira <kakamarpe@gmail.com>
2022-10-04 21:04:51 -0300

    adicionando página de professores

--- aplic/urls.py
+++ aplic/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from aplic.views import IndexView
+from .views import IndexView, SobreView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('sobre/', SobreView.as_view(), name='sobre'),
 
 ]
